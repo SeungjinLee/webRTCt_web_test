7a035d254ce2e0ebf3eda0e72148c5f0c1fa364c
sjlee <reseung21@gmail.com>
2018-11-05 20:37:19 +0900

    11

--- backend/app.js
+++ backend/app.js
@@ -5,37 +5,34 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var apiRouter = require('./routes/api');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'pug'); 
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser()); 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js'))); // redirect bootstrap JS 
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist'))); // redirect JS jQuery
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css'))); // redirect CSS bootstrap
-
-
+ 
 app.use('/', indexRouter);
-app.use('/api/movies', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404)); 
-});
+}); 
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development 
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {}; 
 
   // render the error page
   res.status(err.status || 500);

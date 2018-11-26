12b8e0e7295107827d3b841c550072a01068c62b
sjlee <reseung21@gmail.com>
2018-11-26 23:42:01 +0900

    1

--- backend/routes/socket.js
+++ backend/routes/socket.js
@@ -1,23 +1,31 @@
+var format = require('date-format');
+
 var connectList =[];
 module.exports = {
   start : function(io){
-    io.of('/chat').on('connection', function(socket) {
+    var chat = io.of('/chat').on('connection', function(socket) {
       socket.on('enter', function(data) {
-        console.log("enter : "+socket.id);
-        console.log(data);
-        connectList.push({sid:socket.id, nickname:data.nickname});
-        console.log(connectList);
+        connectList.push({
+          sid : socket.id, 
+          nickname : data.nickname,
+          join_date : format.asString('hh:mm', new Date()),
+          call_check : false
+        });
+        console.log(connectList); 
+        chat.emit("enter", connectList);
       });
       socket.on('forceDisconnect', function() {
         socket.disconnect();
         var pos = connectList.findIndex(x => x.sid === socket.id);
         connectList.splice(pos, 1);
         console.log('forceDisconnect' + socket.id);
-      })
+        chat.emit("enter", connectList);
+      });
       socket.on('disconnect', function() {
         var pos = connectList.findIndex(x => x.sid === socket.id);
         connectList.splice(pos, 1);
         console.log('user disconnected: ' + socket.id);
+        chat.emit("enter", connectList);
       }); 
     });
   },
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,17 +1,38 @@
 var express = require('express');
+var socketEvents = require('../routes/socket.js');
 var router = express.Router();
 
 //session check
 router.use(function (req, res, next) {
+  //console.log(socketEvents.getConnectList());
   console.log("req.session.nickname : "+req.session.nickname);
-  next();
+  if(req.session.nickname){
+    next();
+  }else{
+    res.redirect('/');
+  }
+});
+
+router.get('/exit', function(req, res, next) {
+  if(req.session.nickname){
+      req.session.destroy(function(err){
+          if(err){
+              console.log(err);
+          }else{
+              res.redirect('/');
+          }
+      })
+  }else{
+      res.redirect('/');
+  }
 });
 
 router.post('/list', function(req, res, next) {
   nickname = req.body["nickname"];
-  console.log(req.body);
   req.session.user = nickname;
-  res.render('user_list', { nickname : nickname });
+  res.render('user_list', { 
+    nickname : nickname
+  });
 });
 
 module.exports = router;
